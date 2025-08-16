493dae8b72382c7f525b732317bb522dcfef80e5
Bibin Joseph <bibintomj@gmail.com>
2025-08-16 12:15:29 -0400

    Added Comments

--- src/pages/details/DetailsBanner/detailsBanner.jsx
+++ src/pages/details/DetailsBanner/detailsBanner.jsx
@@ -1,3 +1,4 @@
+// author - Jithin Thomas Jacob
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
--- src/pages/details/cast/Cast.jsx
+++ src/pages/details/cast/Cast.jsx
@@ -1,3 +1,4 @@
+// author - Jithin Thomas Jacob
 import React from "react";
 import { useSelector } from "react-redux";
 import "./style.scss";
